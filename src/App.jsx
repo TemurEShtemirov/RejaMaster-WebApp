7114f1bb2084ac421257b6770e4a135540511fe8
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-07-15 13:04:37 +0500

    fixed router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TaskPage from './pages/TaskPage';
+// import TaskPage from './pages/TaskPage';
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { useState } from 'react';
 
 const App = () => {
+
+  const [taskCreated, setTaskCreated] = useState(false);
+  const [taskDeleted, setTaskDeleted] = useState(false);
+  const [taskUpdated, setTaskUpdated] = useState(false);
+
+  const handleTaskCreated = () => {
+    setTaskCreated(!taskCreated);
+  };
+
+  const handleTaskDeleted = () => {
+    setTaskDeleted(!taskDeleted);
+  };
+
+  const handleTaskUpdated = () => {
+    setTaskUpdated(!taskUpdated);
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TaskPage />} />
+        <Route path="/form" element={<TaskForm onTaskCreated={handleTaskCreated} />
+        } />
+        <Route path="/list" element={<TaskList
+          onTaskDeleted={handleTaskDeleted}
+          onTaskUpdated={handleTaskUpdated}
+        />} />
       </Routes>
     </Router>
   );
